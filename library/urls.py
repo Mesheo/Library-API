36dd874f933d9cf255d34fad1cda5b5c758cef47
Michel <m.ledig.mich@gmail.com>
2021-07-30 11:38:01 -0300

    Pronto pra deploy

--- library/urls.py
+++ library/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from books.api import viewsets as booksviewsets
+from ..books.api import viewsets as booksviewsets
 #criando nosso objeto de rota
 route = routers.DefaultRouter()
 route.register(r'books', booksviewsets.BooksViewSet, basename="Books")
